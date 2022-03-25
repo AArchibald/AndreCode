3087da79cf9681e473071bae5c801af9d6ec37a0
Andre Archibald <97483042+AArchibald@users.noreply.github.com>
2022-03-25 04:48:40 -0400

    Add files via upload

---
+++ Module 10.R
@@ -0,0 +1,10 @@
+Package: SFVStats
+Title: Street Fighter V Odds Calulator
+Version: 0.0.0.9000
+Authors: Andre Archibald <andre19@usf.edu> 
+Description: This package allows one to see the odds of a particular character winning 
+             against another character in the fighting game Street Fighter V.
+             Calculations will be determined based on tournament logged data.
+Depends: R (>= 4.1.1)
+License: CC0
+LazyData: true
